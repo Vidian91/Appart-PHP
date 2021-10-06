0a6e5b3cf74ffa60a71e2468637f84558613e822
Ch.Maurence <christian.maurence+github@10mentionweb.org>
2021-10-06 14:47:24 +0200

    Ajout carrousel

--- fiche-annonce.php
+++ fiche-annonce.php
@@ -44,8 +44,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <!-- mon style -->
     <link rel="stylesheet" href="css/style.css">
 
@@ -75,15 +76,35 @@
                     <h5 class="card-title text-center"><?php echo $fiche['title']; ?></h5>
             
                     <div class="card-body d-flex flex-wrap align-items-center">
-                        <?php 
-                        if(count($listPhotos) != 0)
-                            foreach ( $listPhotos as $photo) 
-                                echo "<div class='flex-shrink-1 ms-3'><img  src='./upload/$photo' alt='$photo' ></div>" ;
-                        else
-                            echo "<img src='./$photo' alt='$photo' >" ;
-                        
-                        //   echo "<img src='./upload/$photo' alt='interieur/le bon appart' class='card-img-top'>" ;
-                          ?>
+
+                    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-interval="2000" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <?php 
+                            if(count($listPhotos) != 0) {
+                                $active = "active" ;
+                                foreach ( $listPhotos as $photo) {
+                                    //echo "<div class='flex-shrink-1 ms-3'><img  src='./upload/$photo' alt='$photo' ></div>" ;
+                                    echo "<div class='carousel-item d-block $active'>
+                                            <img src='./upload/$photo' class='d-block w-100' alt='$photo'>
+                                            </div>";
+                                    if ($active != "") $active = "" ;
+                                }
+                            }
+                            else 
+                                echo "<img src='./$photo' alt='$photo' >" ;
+                            
+                            //   echo "<img src='./upload/$photo' alt='interieur/le bon appart' class='card-img-top'>" ;
+                            ?>
+                            </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
                         <p class="card-text"><?php 
                         echo "<strong>Description</strong> : " .ucfirst($fiche['description']). "<br><strong> Ville</strong> : " .ucfirst($fiche['city']). "<br><strong>Code postal :</strong> " .$fiche['postal_code']. "<br><strong>Type de bien :</strong> ";
                         if($fiche['type'] == 'location') {
